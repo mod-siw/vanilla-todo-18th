3b1d31a36459d7e4f0dcaa4421339e1e7d920d73
mod_siw <pvq_71@ewhain.net>
2023-09-14 23:08:41 +0900

    feat : todo 삭제 ( 이동 X )

--- todoList.js
+++ todoList.js
@@ -5,6 +5,8 @@ const init = () => {
   form.addEventListener("submit", addTodoItem);
 };
 
+/* todo 추가 + 삭제 기능 */
+
 // 할 일 추가 함수
 const addTodoItem = () => {
   event.preventDefault();
@@ -21,17 +23,30 @@ const addTodoItem = () => {
 const printTodoItem = (text) => {
   const todoItem = document.createElement("li");
   const todoText = document.createElement("span");
+  const todoDel = document.createElement("button");
 
   todoItem.className = "todo-list-item";
   todoText.className = "todo-item-text";
   todoText.innerText = text;
 
+  // todo 삭제 + 이동
+  todoDel.innerText = "X";
+  todoDel.className = "todo-delete-button";
+  todoDel.addEventListener("click", deleteTodoItem);
+
   // li에 item 추가
   todoItem.appendChild(todoText);
+  todoItem.appendChild(todoDel);
   document.querySelector(".todo-list").appendChild(todoItem);
 
   // input 창 초기화
   document.querySelector(".todo-input").value = "";
+};
+
+// 할 일 삭제
+const deleteTodoItem = (e) => {
+  const target = e.target.parentNode; //해당 todo item 다 삭제해야 하므로 이동
+  document.querySelector(".todo-list").removeChild(target);
 };
 
 // 시작 함수
